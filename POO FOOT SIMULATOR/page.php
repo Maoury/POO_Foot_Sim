111cab2c54d287083251cf02291eea02391217c3
Squaaw <damien.jisseau@edu.itescia.fr>
2019-02-07 17:21:13 +0100

    version alpha + affichage

--- POO FOOT SIMULATOR/page.php
+++ POO FOOT SIMULATOR/page.php
@@ -27,117 +27,36 @@ require 'Defenseur.php';
    <p>
   	<?php 
        
-    $arsenul = new Equipe('Arsenul');
-// on peut instancier un objet avec son constructeur dans différents endroits
-// ILLUSTRATION:
-$arsenul->setCompo(new Attaquant('Henry', rand(10, 90), 89, 80, rand(10, 30), 75));
-$arsenul->setCompo(new Attaquant('Bob', rand(0, 98), 78, rand(10, 90), 70, 75));
-$arsenul->setCompo(new Attaquant('Fred', 98, rand(0, 78), 80, rand(10, 30), rand(10, 75)));
+    // On peut instancier un objet avec son constructeur dans différents endroits
 
-echo"La composition de léquipe d'arsenul -->";
-echo"<br>";
-//$arsenul->afficherCompo();
-//Pour afficher un certain joueur on utilise les coordonnée du tableaux afin de selectionner l'item désiré et regarder son contenu avec la méthode adéquate
+// On initialise la première équipe
+$arsenul = new Equipe('Arsenul');
 
-//Pour le goal....
-echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Gardien : ';
-echo $arsenul->getCompo()[0]->getGeneralJoueur().'<br>';
-//echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Gardien : ';
-//echo $arsenul->getCompo()[0]->getPosteJoueur().'<br>';
-echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Endurance : ';
-echo $arsenul->getCompo()[0]->getEnduranceJoueur().'<br>';
-echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Vitesse : ';
-echo $arsenul->getCompo()[0]->getVitesseJoueur().'<br>';
-echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Dribble : ';
-echo $arsenul->getCompo()[0]->getDribbleJoueur().'<br>';
-echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Tir : ';
-echo $arsenul->getCompo()[0]->getTirJoueur().'<br>';
+// On initialise les joueurs de cette équipe
+$arsenul->setCompo(new Attaquant('Henry', 98, 89, 80, 70, 35));
+$arsenul->setCompo(new Attaquant('Bob', 58, 78, 80, 70, 90));
+$arsenul->setCompo(new Attaquant('Fred', 98, 89, 80, 70, 85));
 
-echo"<br>";
-//Pour le Defenseur....
-echo $arsenul->getCompo()[1]->getNomJoueur().' --> Defenseur : ';
-echo $arsenul->getCompo()[1]->getGeneralJoueur().'<br>';
-//echo $arsenul->getCompo()[1]->getNomJoueur().'  --> Defenseur : ';
-//echo $arsenul->getCompo()[1]->getPosteJoueur().'<br>';
-echo $arsenul->getCompo()[1]->getNomJoueur().'  --> Endurance : ';
-echo $arsenul->getCompo()[1]->getEnduranceJoueur().'<br>';
-echo $arsenul->getCompo()[1]->getNomJoueur().'  --> Vitesse : ';
-echo $arsenul->getCompo()[1]->getVitesseJoueur().'<br>';
-echo $arsenul->getCompo()[1]->getNomJoueur().'  --> Dribble : ';
-echo $arsenul->getCompo()[1]->getDribbleJoueur().'<br>';
-echo $arsenul->getCompo()[1]->getNomJoueur().'  --> Tir : ';
-echo $arsenul->getCompo()[1]->getTirJoueur().'<br>';
+// On affiche la composition de l'équipe
+echo 'Composition de l\'équipe ' . $arsenul->getNomEquipe() . ' (Moyenne stats : ' . $arsenul->calculMoyenne() . ') : <br> <br>';
+$arsenul->afficherCompo();
+echo '<br> Meilleur buteur : ';
+echo $arsenul->nomMeilleurTireur() . ' (' . $arsenul->meilleurTireur() . ') <br>';
+echo"<br> <br>";
 
-echo"<br>";
-//Pour le Attaquant....
-echo $arsenul->getCompo()[2]->getNomJoueur().' --> Attaquant : ';
-echo $arsenul->getCompo()[2]->getGeneralJoueur().'<br>';
-//echo $arsenul->getCompo()[2]->getNomJoueur().'  --> Defenseur : ';
-//echo $arsenul->getCompo()[2]->getPosteJoueur().'<br>';
-echo $arsenul->getCompo()[2]->getNomJoueur().'  --> Endurance : ';
-echo $arsenul->getCompo()[2]->getEnduranceJoueur().'<br>';
-echo $arsenul->getCompo()[2]->getNomJoueur().'  --> Vitesse : ';
-echo $arsenul->getCompo()[2]->getVitesseJoueur().'<br>';
-echo $arsenul->getCompo()[2]->getNomJoueur().'  --> Dribble : ';
-echo $arsenul->getCompo()[2]->getDribbleJoueur().'<br>';
-echo $arsenul->getCompo()[2]->getNomJoueur().'  --> Tir : ';
-echo $arsenul->getCompo()[2]->getTirJoueur().'<br>';
 
-echo"<br>";
- 
+// On initialise la seconde équipe ainsi que ses joueurs puis on affiche sa composition
 $barca = new Equipe('barca');
-//on peut instancier un objet avec son constructeur dans différents endroits
-// ILLUSTRATION:
-$barca->setCompo(new Attaquant('Messi', rand(10, 100), rand(10, 30), 90, 76, rand(10, 40)));
-$barca->setCompo(new Attaquant('Boubou', rand(20, 100), 98, 79, rand(10, 60), 75));
-$barca->setCompo(new Attaquant('Sala', rand(0, 100), 79, rand(10, 20), 70, rand(10, 90)));
-echo"La composition de léquipe du barca -->";
-echo"<br>";
-//$barca->afficherCompo();
-//Pour afficher un certain joueur on utilise les coordonnée du tableaux afin de selectionner l'item désiré et regarder son contenu avec la méthode adéquate
-//Pour le goal....
-echo $barca->getCompo()[0]->getNomJoueur().'  --> Gardien : ';
-echo $barca->getCompo()[0]->getGeneralJoueur().'<br>';
-//echo $arsenul->getCompo()[0]->getNomJoueur().'  --> Gardien : ';
-//echo $arsenul->getCompo()[0]->getPosteJoueur().'<br>';
-echo $barca->getCompo()[0]->getNomJoueur().'  --> Endurance : ';
-echo $barca->getCompo()[0]->getEnduranceJoueur().'<br>';
-echo $barca->getCompo()[0]->getNomJoueur().'  --> Vitesse : ';
-echo $barca->getCompo()[0]->getVitesseJoueur().'<br>';
-echo $barca->getCompo()[0]->getNomJoueur().'  --> Dribble : ';
-echo $barca->getCompo()[0]->getDribbleJoueur().'<br>';
-echo $barca->getCompo()[0]->getNomJoueur().'  --> Tir : ';
-echo $barca->getCompo()[0]->getTirJoueur().'<br>';
 
-echo"<br>";
-//Pour le Defenseur....
-echo $barca->getCompo()[1]->getNomJoueur().' --> Defenseur : ';
-echo $barca->getCompo()[1]->getGeneralJoueur().'<br>';
-//echo $arsenul->getCompo()[1]->getNomJoueur().'  --> Defenseur : ';
-//echo $arsenul->getCompo()[1]->getPosteJoueur().'<br>';
-echo $barca->getCompo()[1]->getNomJoueur().'  --> Endurance : ';
-echo $barca->getCompo()[1]->getEnduranceJoueur().'<br>';
-echo $barca->getCompo()[1]->getNomJoueur().'  --> Vitesse : ';
-echo $barca->getCompo()[1]->getVitesseJoueur().'<br>';
-echo $barca->getCompo()[1]->getNomJoueur().'  --> Dribble : ';
-echo $barca->getCompo()[1]->getDribbleJoueur().'<br>';
-echo $barca->getCompo()[1]->getNomJoueur().'  --> Tir : ';
-echo $barca->getCompo()[1]->getTirJoueur().'<br>';
+$barca->setCompo(new Attaquant('Messi', 95, 89, 90, 76, 79));
+$barca->setCompo(new Attaquant('Boubou', 68, 68, 70, 50, 72));
+$barca->setCompo(new Attaquant('Sala', 92, 79, 85, 70, 77));
 
-echo"<br>";
-//Pour le Attaquant....
-echo $barca->getCompo()[2]->getNomJoueur().' --> Attaquant : ';
-echo $barca->getCompo()[2]->getGeneralJoueur().'<br>';
-//echo $arsenul->getCompo()[2]->getNomJoueur().'  --> Defenseur : ';
-//echo $arsenul->getCompo()[2]->getPosteJoueur().'<br>';
-echo $barca->getCompo()[2]->getNomJoueur().'  --> Endurance : ';
-echo $barca->getCompo()[2]->getEnduranceJoueur().'<br>';
-echo $barca->getCompo()[2]->getNomJoueur().'  --> Vitesse : ';
-echo $barca->getCompo()[2]->getVitesseJoueur().'<br>';
-echo $barca->getCompo()[2]->getNomJoueur().'  --> Dribble : ';
-echo $barca->getCompo()[2]->getDribbleJoueur().'<br>';
-echo $barca->getCompo()[2]->getNomJoueur().'  --> Tir : ';
-echo $barca->getCompo()[2]->getTirJoueur().'<br>';
+echo 'Composition de l\'équipe ' . $barca->getNomEquipe() . ' (Moyenne stats : ' . $barca->calculMoyenne() . ') : <br> <br>';
+$barca->afficherCompo();
+echo '<br> Meilleur buteur : ';
+echo $barca->nomMeilleurTireur() . ' (' . $barca->meilleurTireur() . ') <br>';
+echo"<br> <br>";
 
     ?>
   </p>
@@ -148,172 +67,125 @@ echo $barca->getCompo()[2]->getTirJoueur().'<br>';
       
        <div id="deroulement">
        <?php
-       //fonction pour dire quelle equipe commence...
-function matchCommence($equipe1, $equipe2)
-{
-         echo"<br>";
-         echo "<h2><----------------    le match commence   -------------></h2>";
-         echo"<br>";
-    
-     
-    
-    if($equipe1->getCompo()[0]->getPhysiqueJoueur() < 
-       $equipe2->getCompo()[0]->getPhysiqueJoueur()) 
-    
-    {
-        echo"<br>";
-        echo "<-------- La balle est pour l'équipe du Arsenul -------->"; 
-        echo"<br>";
-    }
+       function Match (Equipe $equipe1, Equipe $equipe2){
 
-    elseif ($equipe1->getCompo()[0]->getPhysiqueJoueur() > 
-       $equipe2->getCompo()[0]->getPhysiqueJoueur())
-    
-    {   
-        echo"<br>";
-        echo "<--------  La balle est pour l'équipe du barca ----------->";
-        echo"<br>";
-    }
-     
-     else { "<------ Le match est trés sérré ------->";}
-    
-
-
-}
-matchCommence($barca, $arsenul);
-
-
-//fonction pour dire que le match continue...
-function matchContinue($equipe1, $equipe2)
-{
-         echo"<br>";
-         echo "<---------------  le match continue  ------------------->";
-         echo"<br>";
-    
-     
-    
-    if($equipe1->getCompo()[0]->getGeneralJoueur() + 
-       $equipe1->getCompo()[1]->getGeneralJoueur() + 
-       $equipe1->getCompo()[2]->getGeneralJoueur() < 
-       $equipe2->getCompo()[0]->getGeneralJoueur() + 
-       $equipe2->getCompo()[1]->getGeneralJoueur() + 
-       $equipe2->getCompo()[2]->getGeneralJoueur()) 
-    
-    {
-        echo"<br>";
-         echo "<-------  le match continue et c'est l'équipe d' Arsenul qui reprend l'avantage  ------->";
-         echo"<br>";
-    }
-
-    elseif ($equipe1->getCompo()[0]->getGeneralJoueur() + 
-            $equipe1->getCompo()[1]->getGeneralJoueur() + 
-            $equipe1->getCompo()[2]->getGeneralJoueur() > 
-            $equipe2->getCompo()[0]->getGeneralJoueur() + 
-            $equipe2->getCompo()[1]->getGeneralJoueur() + 
-            $equipe2->getCompo()[2]->getGeneralJoueur())
-    
-    {   
-        echo"<br>";
-         echo "<------  le match continue et c'est l'équipe du barca qui reprend l'avantage  ------->";
-         echo"<br>";
-    }
-     
-     else { " Le matche continue----->";}
-    
-
-
-}
-matchContinue($barca, $arsenul);
-
-
-//fonction pour dire qui a finalement la balle
-function matchCommence1($equipe1, $equipe2)
-{
-         echo"<br>";
-         echo "<---------------------  le match est trés mitigé  -------------------->";
-         echo"<br>";
-    
-     
-    
-    if($equipe1->getCompo()[0]->getPhysiqueJoueur() + 
-       $equipe1->getCompo()[1]->getPhysiqueJoueur() + 
-       $equipe1->getCompo()[2]->getPhysiqueJoueur() < 
-       $equipe2->getCompo()[0]->getPhysiqueJoueur() + 
-       $equipe2->getCompo()[1]->getPhysiqueJoueur() + 
-       $equipe2->getCompo()[2]->getPhysiqueJoueur()) 
-    
-    {
-        echo"<br>";
-        echo "<--------  Mais c'est toujours l'equipe d'Arsenul qui a la balle  -------->"; 
-        echo"<br>";
-    }
-
-    elseif ($equipe1->getCompo()[0]->getPhysiqueJoueur() + 
-            $equipe1->getCompo()[1]->getPhysiqueJoueur() + 
-            $equipe1->getCompo()[2]->getPhysiqueJoueur() > 
-            $equipe2->getCompo()[0]->getPhysiqueJoueur() + 
-            $equipe2->getCompo()[1]->getPhysiqueJoueur() + 
-            $equipe2->getCompo()[2]->getPhysiqueJoueur())
-    
-    {   
-        echo"<br>";
-        echo "<---------- Mais c'est toujours l'equipe du barca qui a la balle   ------------>";
-        echo"<br>";
-    }
-     
-     else { " Le match est trés sérré   ---------------->";}
-    
-
-}
-matchCommence1($barca, $arsenul);
-
-       ?>
-           
-       </div>
-   <div id="deroulement1">
-       
-       <?php
-     //fonction de l'equipe qui guagne avec un randum des joueurs...
- function equipeGenerImagGagne1($equipe1, $equipe2)
-{
-         echo"<br>";
-         echo "<---- l'équipe gagnante ---->";
-         echo"<br>";
-    header ("Content-type: png");
-    $image = imagecreatefrompng("barca.png");
-     
-    
-    if($equipe1->getCompo()[0]->getGeneralJoueur() + 
-       $equipe1->getCompo()[1]->getGeneralJoueur() + 
-       $equipe1->getCompo()[2]->getGeneralJoueur() < 
-       $equipe2->getCompo()[0]->getGeneralJoueur() + 
-       $equipe2->getCompo()[1]->getGeneralJoueur() + 
-       $equipe2->getCompo()[2]->getGeneralJoueur()) 
-    
-    {
-        echo"<br>";
-         echo "";
-        echo"<br>";
-         
-    }
-
-    elseif ($equipe1->getCompo()[0]->getGeneralJoueur() + 
-            $equipe1->getCompo()[1]->getGeneralJoueur() + 
-            $equipe1->getCompo()[2]->getGeneralJoueur() > 
-            $equipe2->getCompo()[0]->getGeneralJoueur() + 
-            $equipe2->getCompo()[1]->getGeneralJoueur() + 
-            $equipe2->getCompo()[2]->getGeneralJoueur())
-    
-    {   
-        echo"<br>";
-        echo "";
-        echo"<br>";
-    }
-     
-    //imagepng($image);
-}
-
-equipeGenerImagGagne1($barca, $arsenul);
+        //INITIALISATION DES VARIABLES
+          $turn = "A";
+          $score1 = 0;
+          $score2 = 0;
+          $moyennePasse1 = $equipe1->calculMoyennePasse();
+          $moyennePasse2 = $equipe2->calculMoyennePasse();
+          //LANCEMENT DE LA BOUCLE DE TOUTES LES OCCASION
+          for ($occaz=5; $occaz > 0 ; $occaz--)
+          {
+        
+            // SI C EST A L EQUIPE 1 DE JOUER
+            if ($turn == 'A')
+            {
+              $compteurPasse = 0;
+              $nbpasse= rand(4, 10);
+              //on fais le test de passe max pour arriver au but adverse
+              for ($compteurPasse; $compteurPasse < $nbpasse; $compteurPasse++)
+              {
+                if($moyennePasse1 < mt_rand(0,100))
+                {
+                  echo $equipe1->getNomEquipe(). ' à perdu le ballon!<br>';
+                  //on arrête la boucle si la balle est perdue
+                  break;
+                }
+                else {
+                  echo $equipe1->getNomEquipe() . ' avance sur le terrain!<br>';
+                  echo $compteurPasse .' '. $nbpasse .'<br>';
+                }
+                  }
+                  //Si le nombre de passes n'est pas le bon
+                  if($compteurPasse != $nbpasse)
+                  {
+                    echo '<br>c\'est à l\'equipe '. $equipe2->getNomEquipe(). ' d\'attaquer <br>';
+                    $turn = 'B';
+                  }
+                  //Sinon
+                  else
+                  {
+                    if ($equipe1->meilleurTireur() > mt_rand(0, 100) )
+                    {
+                    echo ' GOOOOOOOOOOOOOOAL '.$equipe1->nomMeilleurTireur(). ' MARQUE CE BUT DE RÊVE<br>';
+                      $score1++;
+                      echo 'c\'est à l\'equipe '. $equipe2->getNomEquipe(). ' d\'attaquer <br>';
+                      $turn = 'B';
+                    }
+                    else
+                     {
+                       echo "C'est dommage ". $equipe1->nomMeilleurTireur().' à totalement raté son tir <br>';
+                       echo '<br>c\'est à l\'equipe '. $equipe2->getNomEquipe(). ' d\'attaquer <br>';
+                       $turn = 'B';
+                    }
+              }
+            }
+            //Au tour de la deuxieme equipe de faire une OCCASION
+            if ($turn == 'B')
+            {
+              $compteurPasse = 0;
+              $nbpasse= rand(4, 10);
+              //on fais le test de passe max pour arriver au but adverse
+              for ($compteurPasse; $compteurPasse < $nbpasse; $compteurPasse++)
+              {
+                if($moyennePasse2 < mt_rand(0,100))
+                {
+                  echo $equipe2->getNomEquipe(). ' à perdu le ballon!<br>';
+                  //on arrête la boucle si la balle est perdue
+                  break;
+                }
+                else {
+                  echo $equipe2->getNomEquipe() . ' avance sur le terrain!<br>';
+                  echo $compteurPasse .' '. $nbpasse .'<br>';
+                }
+                //Si le nombre de passes n'est pas le bon
+        
+                  }
+        
+        
+                  if($compteurPasse != $nbpasse)
+                  {
+                    echo '<br>c\'est à l\'equipe '. $equipe1->getNomEquipe(). ' d\'attaquer <br>';
+                    $turn = 'A';
+                  }
+                  //Sinon
+                  else
+                  {
+                    // ON effectue un test pour verifier si le joueur va marquer après son tir
+                    if ($equipe2->meilleurTireur() > mt_rand(0, 100) )
+                    {
+                      echo ' GOOOOOOOOOOOOOOAL '. $equipe2->nomMeilleurTireur(). ' MARQUE CE BUT DE RÊVE<br>';
+                      $score2++;
+                      echo '<br>c\'est à l\'equipe  '. $equipe1->getNomEquipe(). ' d\'attaquer <br>';
+                      $turn = 'A';
+                }
+                //Sinon
+                else {
+                   echo "C'est dommage ". $equipe2->nomMeilleurTireur().' à totalement raté son tir <br>';
+                   echo '<br>c\'est au '. $equipe1->getNomEquipe(). ' d\'attaquer <br>';
+                   $turn = 'A';
+                }
+              }
+            }
+          }
+          echo $equipe1->getNomEquipe().' '. $score1.' - '.$equipe2->getNomEquipe().' '. $score2;
+          if($score1 > $score2)
+          {
+            echo '<br>C\'est '. $equipe1->getNomEquipe() . ' qui remporte ce match';
+          }
+          else if ($score1 < $score2)
+          {
+            echo '<br>C\'est '. $equipe2->getNomEquipe() . ' qui remporte ce match';
+          }
+          else
+          {
+            echo '<br>C\'est un match nul ';
+        
+          }
+        }
+        match($barca, $arsenul);
      ?>
      
    </div>     
